96497f98a3e3f76aa75da9b11b403e9be6bb49ae
Amit Kumar Gupta <amitgupta542173@gmail.com>
2025-06-14 02:45:43 +0530

    tailwind setup and basic UI typing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import { Button } from './components/ui/Button'
+import { PlusIcon } from './icons/PlusIcon'
 
 function App() {
 
 
   return (
     <>
-      <h1 className='text-green bg-blue-300 rounded-xl' >This is a react App intitialised with vite and typescript.</h1>
+      <h1 className='text-primary bg-blue-300 rounded-xl' >This is a react App intitialised with vite and typescript.</h1>
+      <Button startIcon={<PlusIcon/>} variant="primary"  size="sm" text="Hello" onClick={() => { console.log("button clikced") }} />
+      <Button variant="secondary" size="md" text="Hello" onClick={() => { console.log("button clikced") }} />
+      <Button variant="secondary" size="lg" text="Hello" onClick={() => { console.log("button clikced") }} />
+
+
     </>
   )
 }
--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,18 +1,36 @@
 import type { ReactElement } from "react";
 
-export interface ButtonProps{
-    variant : "primary" | "secondary";
-    size:"sm" | "md" | "lg";
+
+export interface ButtonProps {
+    variant: "primary" | "secondary";
+    size: "sm" | "md" | "lg";
     text: string,
-    startIcon : ReactElement;
-    endIcon: ReactElement;
-
+    startIcon?: ReactElement | null;
+    endIcon?: ReactElement | null;
+    onClick: () => void;
 }
 
-export const Button = (props : ButtonProps) => {
-    return(
-        <div>
-            
-        </div>
+
+
+const VariantStyles = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-300 text-purple-600 ",
+}
+
+const defaultStyles = "rounded-md m-2 p-4 flex"
+
+const sizeStyles= {
+    "sm" : "px-1 py-1",
+    "md" : "px-2 py-2",
+    "lg" : "px-4 py-3"
+}
+
+
+
+export const Button = (props: ButtonProps) => {
+    console.log(props);
+    return (
+        <button className={`${VariantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`} >{props.startIcon} {props.text} {props.endIcon}</button>
     )
-}
+}   
+ 
