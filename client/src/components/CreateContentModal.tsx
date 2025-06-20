50d85b6ef3c8cc3e678988d06326f47fc0161e84
Amit Kumar Gupta <amitgupta542173@gmail.com>
2025-06-20 23:02:11 +0530

    modal box

---
+++ client/src/components/CreateContentModal.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+export function CreateContentModal({open , onClose}){
+    const [modalOpen , setModalOpen] = useState(false);
+
+    return(
+        <div>
+            <CrossIcon onClick = {() => {}}/>
+        </div>
+    )
+}
